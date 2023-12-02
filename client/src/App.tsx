4329d0e04de41f0cb4df381454decb1ccae09cab
scottvphan <114790711+scottvphan@users.noreply.github.com>
2023-12-02 00:56:37 -0500

    added global styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,52 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import { Layout } from './components/Layout'
-import { Home } from './pages/Home'
-import { Services } from './pages/Services'
-import { Contact } from './pages/Contact'
+import { useState } from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Services } from "./pages/Services";
+import { Contact } from "./pages/Contact";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
+    const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  :root {
+      margin: 0;
+      padding: 0;
+      font-family:'Jost', Arial, Helvetica, sans-serif;
+      scroll-behavior: smooth;
+  }
+  button {
+      font-family:'Jost', Arial, Helvetica, sans-serif;
+      cursor:pointer;
+  }
+  img{
+      user-select: none;
+  }
+`;
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
+            </Route>
+        )
+    );
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/contact' element={<Contact />} />
-      </Route>
-    )
-  )
-
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  )
+    return (
+        <>
+            <GlobalStyle />
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
-export default App
+export default App;
