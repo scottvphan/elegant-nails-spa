08fd023401aa42e222411593c93f77e55a769d80
scottvphan <114790711+scottvphan@users.noreply.github.com>
2023-12-14 22:42:19 -0500

    added server

--- server/server.ts
+++ server/server.ts
@@ -7,21 +7,22 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiKey = process.env.GOOGLE_API_KEY;
+const googleApiKey = process.env.GOOGLE_API_KEY;
 const placeId = process.env.GOOGLE_PLACE_ID;
 
 app.use(express.json());
 
 app.get("/google-reviews", async (req: Request, res: Response) => {
-  if (!apiKey || !placeId) {
+  if (!googleApiKey || !placeId) {
     return res.status(500).json({ error: "API key or place ID not provided." });
   }
 
-  const placeDetailsUrl = `https://places.googleapis.com/v1/places/ChIJj61dQgK6j4AR4GeTYWZsKWw?fields=reviews&key=AIzaSyBBW2F1HMcu3yJ1wG2sHo4eRoelcb2jlVY`;
+  const placeDetailsUrl = `https://places.googleapis.com/v1/places/${placeId}?fields=reviews&key=${googleApiKey}`;
   try {
     const placeDetailsResponse = await axios.get(placeDetailsUrl);
-    const reviews = placeDetailsResponse.data.reviews ? placeDetailsResponse.data.reviews || [] : [];
-
+    const reviews = placeDetailsResponse.data.reviews
+      ? placeDetailsResponse.data.reviews || []
+      : [];
     res.json({ reviews });
   } catch (error) {
     console.error("Error fetching Google Reviews:", error);
