20b03a73f1021065ff87cf229ffdc5914e5cb18e
scottvphan <114790711+scottvphan@users.noreply.github.com>
2024-01-07 13:49:01 -0500

    created build for frontend

--- client/src/components/ServicesComponent.tsx
+++ client/src/components/ServicesComponent.tsx
@@ -1,26 +1,5 @@
 import styled from "styled-components";
 
-const PricingHeader = styled.h2`
-  color: #000;
-  font-family: Jost;
-  font-size: 5rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px; /* 75% */
-`;
-const FancyFont = styled.h5`
-  color: #f9b698;
-  text-align: center;
-  font-family: Euphoria Script;
-  font-size: 4rem;
-  font-style: normal;
-  font-weight: 400;
-`;
-const ServiceHeadingContainer = styled.div`
-  display: grid;
-  text-align: center;
-  gap: 1rem;
-`;
 const ServiceContainer = styled.div`
   display: flex;
   justify-content: space-between;
@@ -28,10 +7,6 @@ const ServiceContainer = styled.div`
   box-sizing: border-box;
   border-bottom:1px dotted black;
   align-items: center;
-`;
-const ServiceLayoutContainer = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
 `;
 const ServiceName = styled.p`
   font-size: 1rem;
