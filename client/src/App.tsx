833ea8546eb58ef17d8c4e1b2342bc18be50050f
scottvphan <114790711+scottvphan@users.noreply.github.com>
2024-01-07 18:35:04 -0500

    added login to access site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,8 +9,53 @@ import { Home } from "./pages/Home";
 import { Services } from "./pages/Services";
 import { Contact } from "./pages/Contact";
 import { createGlobalStyle } from "styled-components";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [hasAccess, setHasAccess] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  useEffect(() => {
+    const accessLocalStorage = getLocalStorage("access");
+    if (accessLocalStorage) {
+      setHasAccess(true);
+    }
+  }, []);
+
+  const handleLogin = (): void => {
+    if (username === "elegantnails" && password === "phan") {
+      setLocalStorage("access", "true", 24);
+      setHasAccess(true);
+    }
+  };
+
+  const setLocalStorage = (
+    name: string,
+    value: string,
+    hours: number
+  ): void => {
+    const expirationTime = new Date().getTime() + hours * 60 * 60 * 1000;
+    localStorage.setItem(name, JSON.stringify({ value, expirationTime }));
+  };
+
+  const getLocalStorage = (name: string): string | null => {
+    const item = localStorage.getItem(name);
+    if (item) {
+      const parsedItem = JSON.parse(item);
+      if (parsedItem.expirationTime > new Date().getTime()) {
+        return parsedItem.value;
+      } else {
+        removeLocalStorage(name);
+      }
+    }
+    return null;
+  };
+
+  const removeLocalStorage = (name: string): void => {
+    localStorage.removeItem(name);
+  };
+
   const GlobalStyle = createGlobalStyle`
     * {
       box-sizing: border-box;
@@ -50,8 +95,38 @@ function App() {
 
   return (
     <>
-      <GlobalStyle />
-      <RouterProvider router={router} />
+      {hasAccess ? (
+        <>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </>
+      ) : (
+        <>
+          <form>
+            <label>
+              Username:
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        </>
+      )}
     </>
   );
 }
